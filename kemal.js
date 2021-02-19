923a8abbdde221b7f83644236f955a5dbb2184ef
Mekarinkata <77516330+Mekarinkata@users.noreply.github.com>
2021-02-19 12:35:39 +0400

    Add files via upload

---
+++ kemal.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js")     
+const client = new Discord.Client();       
+const config = require("./kemalayar.js")    
+const fs = require("fs");                
+require('./util/Loader.js')(client);     
+
+client.commands = new Discord.Collection(); 
+client.aliases = new Discord.Collection();  
+fs.readdir('./commands/', (err, files) => { 
+  if (err) console.error(err);               
+  console.log(`${files.length} komut yüklenecek.`); 
+  files.forEach(f => {                       
+    let props = require(`./commands/${f}`);   
+    console.log(`${props.config.name} komutu yüklendi.`);  
+    console.log(`kendinKemal`)     
+    client.commands.set(props.config.name, props); 
+    props.config.aliases.forEach(alias => {          
+      client.aliases.set(alias, props.config.name);  
+    });
+  });
+})
+
+client.login(config.token)
+
+
+
+
---
+++ kemalayar.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "token": "", //botunuzun tokeninin yazılması gereken yer
+  "geliştiriciler": [""], //geliştirici ve ya geliştiricilerin İD sinin yazılması gereken yer
+  "prefix": "!" //botunuzun komutundan once gelicek işsaretiniz
+};
